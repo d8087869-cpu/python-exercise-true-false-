cab3db00f1e8b14cf90625d7f0fbabf89095b5e4
david <d8087869@gmail.com>
2026-06-18 18:48:13 +0300

    added_true_false_info

---
+++ logic.py
@@ -0,0 +1,15 @@
+#1 
+is_online = True
+has_access = False
+print(is_online and has_access)
+print(is_online or has_access)
+#3 
+status = False 
+print(not False)
+#4
+age=20 
+has_id =True
+print(age>= 18 and has_id)
+level = 3 
+print(level >=1 and level <=5)
+
