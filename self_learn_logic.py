9dd1e90ec807dfe43db824ae06cce460a446d905
david <d8087869@gmail.com>
2026-06-18 22:15:35 +0300

    added_value_to_binary

---
+++ self_learn_logic.py
@@ -0,0 +1,12 @@
+#1 short-circuit is whaen an anser is none to the "opretor" that the ansewr will be false or true acording to the info that is givein 
+#for exmpale false and true . if false must be false by the bloon=and  / if I undurstand rigth its shown by = if. same if it starts with true or .... 
+#2 and is before or . what you calculate before. 
+#3 == the same 3=3 , 1+2=3 = true 
+#is means that the same not only in the conclusion but also in the qustion 3=3 true , 2+1=3 false 
+#4 x==none could be change . but is it cheking the define . 
+a = 12 
+b = 10
+print(bin(a))
+print(bin(b))
+print(a ^ b)
+print(bin(a ^ b))
